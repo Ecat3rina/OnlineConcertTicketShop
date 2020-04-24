754353403ddf66f1af3dbea4fcc106cbeeb4ce96
Raileanu Ion <iraileanu@iraileanu-iMac.local>
2020-04-24 12:20:53 +0300

    Home layout

--- TicketShopWeb/src/app/app.module.ts
+++ TicketShopWeb/src/app/app.module.ts
@@ -11,6 +11,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [
@@ -26,7 +31,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatSidenavModule,
+    MatListModule,
+    MatDividerModule,
+    MatIconModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TicketShopWeb/src/app/components/home/home.component.ts
+++ TicketShopWeb/src/app/components/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  username : string = "iraileanu";
+
   constructor() { }
 
   ngOnInit(): void {
